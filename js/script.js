2dcc9efdca75c39caebda63e2392a53382a29cff
moraclemoby <krisnaagungwidodo@gmail.com>
2023-06-07 14:08:20 +0700

    layouting

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function validateform(){
+    var text = document.forms["message-form"]["full-name"].value;
+    var date = document.forms["message-form"]["birth-date"].value;
+    var gender = document.forms["message-form"]["gender"].value;
+    var messages = document.forms["message-form"]["messages"].value;
+  
+    if  (text =="" || date == "" || gender == "" || messages == "") {
+      alert("Input tidak boleh kosong");
+      console.log();
+      return false;
+    }
+  
+    document.getElementById("sender-full-name").innerText= text;
+    document.getElementById("sender-birth-date").innerText= date;
+    document.getElementById("sender-gender").innerText=gender;
+    document.getElementById("sender-messages").innerText=messages;
+    console.log();
+    return false;
+  }
