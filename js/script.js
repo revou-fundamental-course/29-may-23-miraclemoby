5fb35cdf6b490b6f812ab1bccd8539ce6f8f6f27
krisnaagungw <isnarev007@gmail.com>
2023-06-08 15:43:45 +0700

    slidefail

--- js/script.js
+++ js/script.js
@@ -17,3 +17,26 @@ function validateform(){
     
     return false;
   }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex +=n));
+}
+
+function showDivs(n) {
+  var i;
+  var x; document.getElementsByClassName("img-slideshow");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1){
+    slideIndex = x.length
+  }
+
+  for (i = 0; i < x.length; i++) {
+    x[1].style.display = "none"
+  }
+  x[slideIndex - 1].style.display = "block"
+}
